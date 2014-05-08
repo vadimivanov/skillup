633a522ead97b01f41f668d0c963089b00abd0e4
Vadim Ivanov <v.ivanov@mobidev.biz>
2014-05-08 18:37:14 +0300

    todo vector

--- example.js
+++ example.js
@@ -94,4 +94,35 @@ function draw(ballNext) {
     }
 }
 
+//vector coordinate
+
+//Collider.prototype.collideSphereVsSphere = function (s1, s2, dt) {
+//    var s2x = s2.x + (s2.velocity.x * dt/1000),
+//        s2y = s2.y + (s2.velocity.y * dt/1000),
+//        s1x = s1.x + (s1.velocity.x * dt/1000),
+//        s1y = s1.y + (s1.velocity.y * dt/1000);
+//    var distance = new Vector(s2x - s1x, s2y - s1y);
+//    var radiusSum = s1.radius + s2.radius;
+//    var isColliding = Math.vDot(distance, distance) <= radiusSum * radiusSum;
+//    var overlap = radiusSum - distance.getMagnitude();
+//
+//    if (!isColliding) {
+//        return false;
+//    }
+//    return {
+//        overlap: overlap,
+//        normal: distance.normalize(),
+//        collidedObj: s2
+//    };        //TODO: Return distance vector & overlap
+//};
+// this.colliderCallback = function (collObj, result) {
+//    var overlapVector = this.velocity.normalize().multiply(Math.ceil(result.overlap)),
+//        normal;
+////        this.x -= overlapVector.x;
+////  this.y -= overlapVector.y;
+////        normal = new Vector(collObj.x - this.x, collObj.y - this.y).normalize();
+//    normal = result.normal;
+//    this.velocity = Math.vSubtr(this.velocity, normal.multiply(2 * Math.vDot(this.velocity, normal)));
+//};
+
 
